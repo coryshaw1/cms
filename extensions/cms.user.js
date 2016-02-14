c247a7667b2354cf42947ac2a962e1b4f1d5fd42
possiblyKris <possiblyKris@gmail.com>
2016-02-14 23:34:22 +0100

    Update userscript for more stable loading.

--- extensions/cms.user.js
+++ extensions/cms.user.js
@@ -4,8 +4,13 @@
 // @description    Runs cms on dubtrack
 // @author         Mitch & Kris
 // @include        https://www.dubtrack.fm/join/*
-// @version        0.5
+// @version        0.5.1
 // @grant          none
 // ==/UserScript==
 
-$.getScript('https://chilloutmusica.github.io/cms/cms.js');
+$(document).ready(function() {
+    setTimeout(function() {
+        console.log('CMS Loaded');
+        $.getScript('https://chilloutmusica.github.io/cms/cms.js');
+    }, 500);
+});
