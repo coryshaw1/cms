be240d659297bf3989ab827f139bdd0f809ba2b1
Mitchdev <admin@mitchdev.net>
2016-04-01 22:55:45 +1300

    Move php from mitchdev to github

---
+++ cm.php
@@ -0,0 +1,10 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+?>
+{
+	"css": "https://chilloutmusica.github.io/cms/assets/cm.css",
+	"social": {
+		"icon": "https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-02-26/23302575793_1bb347d4bf21e4d8e823_132.jpg",
+		"link": "https://chilloutmusic.fm"
+	}
+}
