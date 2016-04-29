d84d5ac2388f55400b4f90715c9ca7eba2e9f9a0
Mitchdev <admin@mitchdev.net>
2016-04-29 12:01:35 +1200

    Auto complete temp disabled due to bugs

--- cms.js
+++ cms.js
@@ -1481,68 +1481,66 @@ function updateuserarray(e) {
     }
 }
 
-function autocompleteue() {
-    if (options.autocomplete) {
-        var emoarray = localStorage.getItem('emoarray').toLowerCase().split(',');
-        emoarray.forEach(function(e) {
-            emo.push('' + e + '');
-        });
-        Dubtrack.room.users.collection.models.forEach(function(e) {
-            var user = e.attributes._user.username;
-            men.push('' + user + '');
-        });
-        (function($) {
-            $('#chat-txt-message').textcomplete([{
-                mentions: men,
-                match: /\B@([\-+\w]*)$/,
-                search: function(term, callback) {
-                    callback($.map(this.mentions, function(mention) {
-                        return mention.toLowerCase().indexOf(term.toLowerCase()) === 0 ? mention : null;
-                    }));
-                },
-                index: 1,
-                template: function(value) {
-                    var id;
-                    Dubtrack.cache.users.getByUsername('' + value + '', function(err, user) {
-                        if (err) {
-                            console.log("user not found");
-                        }
-                        else {
-                            id = user.id;
-                        }
-                    });
-                    return '<img src="https://api.dubtrack.fm/user/' + id + '/image"></img>@' + value + '<span class="a">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;press enter to select</span>';
-                },
-                replace: function(mention) {
-                    return '@' + mention + ' ';
-                }
-            }, {
-                emojies: emo,
-                match: /\B:([\-+\w]*)$/,
-                search: function(term, callback) {
-                    callback($.map(this.emojies, function(emoji) {
-                        return emoji.indexOf(term) === 0 ? emoji : null;
-                    }));
-                },
-                index: 1,
-                template: function(value) {
-                    return '<img src="https://www.dubtrack.fm/assets/emoji/images/emoji/' + value + '.png"></img>:' + value + ':<span class="a">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;press enter to select</span>';
-                },
-                replace: function(value) {
-                    return ':' + value + ': ';
-                }
-            }]);
-        });
-        setInterval(function() {
-            if ($(".textcomplete-dropdown").css('display') == 'block') {
-                $('.pusher-chat-widget-input').find('input').attr('id', 'disabled-for-autocomplete');
-            }
-            else if ($(".textcomplete-dropdown").css('display') == 'none') {
-                $('.pusher-chat-widget-input').find('input').attr('id', 'chat-txt-message');
-            }
-        }, 500);
-    }
-}
+//function autocompleteue() {
+    //if (options.autocomplete) {
+        //var emoarray = localStorage.getItem('emoarray').toLowerCase().split(',');
+        //emoarray.forEach(function(e) {
+            //emo.push('' + e + '');
+        //});
+        //Dubtrack.room.users.collection.models.forEach(function(e) {
+            //var user = e.attributes._user.username;
+            //men.push('' + user + '');
+        //});
+        //$('#chat-txt-message').textcomplete([{
+            //mentions: men,
+            //match: /\B@([\-+\w]*)$/,
+            //search: function(term, callback) {
+                //callback($.map(this.mentions, function(mention) {
+                    //return mention.toLowerCase().indexOf(term.toLowerCase()) === 0 ? mention : null;
+                //}));
+            //},
+            //index: 1,
+            //template: function(value) {
+                //var id;
+                //Dubtrack.cache.users.getByUsername('' + value + '', function(err, user) {
+                    //if (err) {
+                        //console.log("user not found");
+                    //}
+                    //else {
+                        //id = user.id;
+                    //}
+                //});
+                //return '<img src="https://api.dubtrack.fm/user/' + id + '/image"></img>@' + value + '<span class="a">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;press enter to select</span>';
+            //},
+            //replace: function(mention) {
+                //return '@' + mention + ' ';
+            //}
+        //}, {
+            //emojies: emo,
+            //match: /\B:([\-+\w]*)$/,
+            //search: function(term, callback) {
+                //callback($.map(this.emojies, function(emoji) {
+                    //return emoji.indexOf(term) === 0 ? emoji : null;
+               //}));
+            //},
+            //index: 1,
+            //template: function(value) {
+                //return '<img src="https://www.dubtrack.fm/assets/emoji/images/emoji/' + value + '.png"></img>:' + value + ':<span class="a">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;press enter to select</span>';
+            //},
+            //replace: function(value) {
+                //return ':' + value + ': ';
+            //}
+        //}]);
+        //setInterval(function() {
+            //if ($(".textcomplete-dropdown").css('display') == 'block') {
+                //$('.pusher-chat-widget-input').find('input').attr('id', 'disabled-for-autocomplete');
+            //}
+            //else if ($(".textcomplete-dropdown").css('display') == 'none') {
+                //$('.pusher-chat-widget-input').find('input').attr('id', 'chat-txt-message');
+            //}
+        //}, 500);
+    //}
+//}
 
 function notifyonmention(e) {
     if (options.notifionmention) {
